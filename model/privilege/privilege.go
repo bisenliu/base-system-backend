7c63380db7bb6c2dbe813c4067b670a9210cca52
liubisen <www.924436173@qq.com>
2023-05-24 22:20:40 +0800

    fix:repair privilege model filed type error question

--- model/privilege/privilege.go
+++ model/privilege/privilege.go
@@ -7,10 +7,10 @@ import (
 
 type Privilege struct {
 	Id         int64          `gorm:"column:id;primaryKey;autoIncrement;notNull;comment:Id"`
-	Title      int            `gorm:"column:title;notNull;size:20;comment:系统权限名称"`
-	Key        int            `gorm:"column:key;size:50;comment:系统权限key"`
-	Dependency datatypes.JSON `gorm:"column:dependency;comment:依赖权限key"`
+	Title      string         `gorm:"column:title;notNull;size:20;comment:系统权限名称"`
+	Key        string         `gorm:"column:key;size:50;comment:系统权限key"`
 	ParentId   string         `gorm:"column:parent_id;comment:父级Id"`
+	Dependency datatypes.JSON `gorm:"column:dependency;comment:依赖权限key"`
 }
 
 func (receiver Privilege) TableName() string {
