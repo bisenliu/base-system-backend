c23817b696fb7b25f9d9886f8037a8a21f89d892
liubisen <www.924436173@qq.com>
2023-07-15 11:03:37 +0800

    fix:optimization request params verify logic

--- utils/validate/request_params.go
+++ utils/validate/request_params.go
@@ -14,55 +14,62 @@ import (
 	"strings"
 )
 
-func RequestParamsVerify(c *gin.Context, params interface{}) (res bool) {
+func RequestParamsVerify(c *gin.Context, params interface{}) bool {
 	// 绑定请求参数
 	if err := c.ShouldBindBodyWith(&params, binding.JSON); err != nil {
-		global.LOG.Error("invalid request params: ", zap.Error(err))
-		if validateError, ok := err.(validator.ValidationErrors); ok {
-			response.Error(c, code.InvalidParams, removeTopStruct(validateError.Translate(global.TRANS)), err.Error())
-			return
-		}
-		if unmarshalTypeError, ok := err.(*json.UnmarshalTypeError); ok {
-			typeOfCat := reflect.TypeOf(params)
-			//如果是指针类型则转换为非指针
-			if typeOfCat.Kind() == reflect.Ptr {
-				typeOfCat = typeOfCat.Elem()
-			}
-			for i := 0; i < typeOfCat.NumField(); i++ {
-				// 获取每个成员的结构体字段类型
-				fieldType := typeOfCat.Field(i)
-				var (
-					rawFailed string
-					value     string
-				)
-				if unmarshalTypeError.Field == strings.Split(fieldType.Tag.Get("json"), ",")[0] {
-					rawFailed = strings.Split(fieldType.Tag.Get("json"), ",")[0]
-					value = fieldMapping(unmarshalTypeError)
-					if fieldType.Tag.Get("label") != "" {
-						rawFailed = fieldType.Tag.Get("label")
-					}
-					msg := strings.Join([]string{fmt.Sprintf("%s", rawFailed), "字段不合法,", "应为", value}, "")
-					response.Error(c, code.InvalidParams, map[string]interface{}{
-						unmarshalTypeError.Field: msg,
-					}, unmarshalTypeError.Error())
-					return
-				}
-			}
-		} else {
-			response.Error(c, code.InvalidParams, err, err.Error())
-			return
-		}
+		handleError(c, params, err)
+		return false
 	}
 	return true
 }
 
-func fieldMapping(unmarshalTypeError *json.UnmarshalTypeError) string {
-	switch unmarshalTypeError.Type.String() {
-	case "int", "int64", "[]int", "[]int64", "enums.Gender":
-		return "整型"
-	case "string":
-		return "字符串类型"
+func handleError(c *gin.Context, params interface{}, err error) {
+	global.LOG.Error("invalid request params: ", zap.Error(err))
+	switch err := err.(type) {
+	case validator.ValidationErrors:
+		validateError := err
+		response.Error(c, code.InvalidParams, removeTopStruct(validateError.Translate(global.TRANS)), err.Error())
+	case *json.UnmarshalTypeError:
+		unmarshalTypeError := err
+		fieldName := getFieldJSONName(params, err)
+		errorMap := map[string]string{
+			unmarshalTypeError.Field: buildErrorMessage(fieldName, unmarshalTypeError.Type.String()),
+		}
+		response.Error(c, code.InvalidParams, errorMap, unmarshalTypeError.Error())
 	default:
-		return unmarshalTypeError.Type.String()
+		response.Error(c, code.InvalidParams, err, err.Error())
+	}
+}
+
+func getFieldJSONName(params interface{}, unmarshalTypeError *json.UnmarshalTypeError) string {
+	var rawFailed string
+	typeOfCat := reflect.TypeOf(params)
+	//如果是指针类型则转换为非指针
+	if typeOfCat.Kind() == reflect.Ptr {
+		typeOfCat = typeOfCat.Elem()
+	}
+	for i := 0; i < typeOfCat.NumField(); i++ {
+		// 获取每个成员的结构体字段类型
+		fieldType := typeOfCat.Field(i)
+		if unmarshalTypeError.Field == strings.Split(fieldType.Tag.Get("json"), ",")[0] {
+			rawFailed = strings.Split(fieldType.Tag.Get("json"), ",")[0]
+			if fieldType.Tag.Get("label") != "" {
+				rawFailed = fieldType.Tag.Get("label")
+			} else {
+				rawFailed = unmarshalTypeError.Field
+			}
+		}
+	}
+	return rawFailed
+}
+
+func buildErrorMessage(fieldName, fieldType string) string {
+	switch fieldType {
+	case "int", "int64", "[]int", "[]int64", "enums.Gender":
+		return fmt.Sprintf("%s字段不合法，应为整型", fieldName)
+	case "string":
+		return fmt.Sprintf("%s字段不合法，应为字符串类型", fieldName)
+	default:
+		return fmt.Sprintf("%s字段不合法，应为%s类型", fieldName, fieldType)
 	}
 }
